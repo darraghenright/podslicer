034abf945b247e27b4883ff42b1810c9c00e5598
Darragh Enright <darragh@bodhala.com>
2023-09-03 12:33:47 +0100

    For now exit unconditionally on user input

--- podslicer/__main__.py
+++ podslicer/__main__.py
@@ -1,6 +1,14 @@
-from urwid import TOP, Divider, Filler, Frame, MainLoop, Pile, Text  # type: ignore
+from typing import NoReturn
+
+import urwid as u  # type: ignore
+from urwid import Divider, Filler, Frame, MainLoop, Pile, Text  # type: ignore
 
 from podslicer.display import Row
+
+
+def exit_on_input(key: str) -> NoReturn:
+    raise u.ExitMainLoop()
+
 
 header_rows = [
     "<header>podslicer 🔪✨</header>",
@@ -19,9 +27,9 @@ styles = [
 header = Pile([Text(Row.parse(row)) for row in header_rows])
 
 hint = Text("")
-body = Filler(Pile([Divider(), hint]), TOP)
+body = Filler(Pile([Divider(), hint]), u.TOP)
 
 frame = Frame(body, header=header)
 
-loop = MainLoop(frame, styles)
+loop = MainLoop(frame, styles, unhandled_input=exit_on_input)
 loop.run()
